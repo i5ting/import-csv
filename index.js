53f94a1836fb4962015499cf9eebf70ed9b7b3ac
i5ting <i5ting@126.com>
2015-11-01 07:43:39 +0800

    refact

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
 var csv = require('csv')
   , fs = require('fs')
   , iconv = require('iconv-lite');
- 
+
 module.exports = function (file, cb, encode) {
   var _encode = 'gbk';
+
   if (encode) {
     _encode = encode;
   }
 
-  var parser = csv.parse({delimiter: ','}, function(err, data){
+  var parser = csv.parse({ delimiter: ',' }, function (err, data) {
     cb(err, data);
   });
 
-  // update_pay_status_2(pay_num)
-  return fs.createReadStream(file).pipe(iconv.decodeStream(_encode)).pipe(parser);
+  var out = fs.createReadStream(file);
+
+  return out.pipe(iconv.decodeStream(_encode)).pipe(parser);
 }
